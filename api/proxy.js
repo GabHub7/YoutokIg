37ce11ce0460d6bab478054defb1a6e274fa8556
GabHub7 <gabrael9999@gmail.com>
2026-03-13 13:42:30 +0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -122,13 +122,8 @@ module.exports = async function handler(req, res) {
       if (!hd && formats.length > 0) hd = formats[0].url || "";
       if (!sd) sd = hd;
 
-      // Bungkus lewat proxy download biar ga 403
-      const proto = req.headers["x-forwarded-proto"] || "https";
-      const host  = req.headers["x-forwarded-host"] || req.headers.host;
-      const base  = `${proto}://${host}/api/proxy`;
-      const wrap = (u, ext) => u
-        ? `${base}?mode=download&fileurl=${encodeURIComponent(u)}&filename=${encodeURIComponent(data.title || "video")}&ext=${ext}`
-        : "";
+      // Langsung pakai URL asli
+      const wrap = (u, ext) => u || "";
 
       return res.json({
         source:    "youtube",
@@ -147,4 +142,4 @@ module.exports = async function handler(req, res) {
   }
 
   return res.status(400).json({ error: "Request tidak valid" });
-      }
+    }
