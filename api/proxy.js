d970f6cbdc4aab75c7ba32998d02e5d7c200fcd1
GabHub7 <gabrael9999@gmail.com>
2026-03-13 09:32:02 +0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
 
   const RAPIDAPI_KEY = process.env.RAPIDAPI_KEY;
@@ -145,4 +145,4 @@ export default async function handler(req, res) {
   }
 
   return res.status(400).json({ error: "Request tidak valid" });
-           }
+      }
